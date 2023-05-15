5129d3a2154dd0a1a0d88e2edd49b2670e444495
82106 <82106@DESKTOP-G434CV6>
2023-05-15 21:48:41 +0900

    0509

--- j0509/src/j0509/Class0509_04.java
+++ j0509/src/j0509/Class0509_04.java
@@ -23,10 +23,15 @@ public class Class0509_04 {
 		System.out.println("아이디를 입력하세요.");
 		id = scan.nextLine();
 		System.out.println("패스워드를 입력하세요.");
-		pw = scan.nextInt(); // 숫자 0을 넣고 싶은 경우, scan.nextLine()으로 변경, 아래 scan.nextLine(); 필요없음.
-		scan.nextLine(); // nextInt() enter키를 입력으로 사용!하기 때문에 여기를 꼭 적어줘야함!!!
+		pw = scan.nextInt(); 
+		// 숫자 0을 넣고 싶은 경우, scan.nextLine()으로 변경, 아래 scan.nextLine(); 필요없음.
+		scan.nextLine(); 
+		// nextInt()가 enter키를 뱉어내기 때문에 enter를 받을 수 있는 으로 사용!하기 때문에 위를 꼭 적어줘야함!!!
 		System.out.println("이름을 입력하세요.");
 		name = scan.nextLine();
+		
+		// scan.next()와 nextInt()는 띄어쓰기를 받지 않고, nextLine()만 받을 수 있음!
+		
 		
 		// 3. 출력
 		System.out.println("----------------------");
--- j0509/src/j0509/Class0509_09.java
+++ j0509/src/j0509/Class0509_09.java
@@ -16,7 +16,8 @@ public class Class0509_09 {
 		System.out.println("문자열을 입력하세요.");
 
 		char input = scan.next().charAt(0); // charAt() 문자열에서 1개의 문자를 가져옴.
-		int result = input - '0'; // char 타입의 숫자는 '0'문자 0을 빼야 숫자값으로 변환됨. 그냥 0을 빼면 아스키코드로 변환되어 원하지 않는 값 나옴.
+		int result = input - '0'; 
+		// char 타입의 숫자는 '0'문자 0을 빼야 숫자값으로 변환됨. 그냥 0을 빼면 아스키코드로 변환되어 원하지 않는 값 나옴.
 		String content = (result % 2 == 0) ? "짝수입니다." : "홀수입니다.";
 
 		System.out.println("추출한 숫자 : " + result);
