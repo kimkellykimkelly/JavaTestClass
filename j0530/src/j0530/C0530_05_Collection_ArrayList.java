575556db76ebc133ecc501fd5976f8f3f1c80a34
82106 <82106@DESKTOP-G434CV6>
2023-05-31 19:32:43 +0900

    0530

--- j0530/src/j0530/C0530_05_Collection_ArrayList.java
+++ j0530/src/j0530/C0530_05_Collection_ArrayList.java
@@ -1,6 +1,7 @@
 package j0530;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class C0530_05_Collection_ArrayList {
 
@@ -8,13 +9,14 @@ public class C0530_05_Collection_ArrayList {
 		// Wrapper 클래스: 기본형을 클래스로 정의한 것! 기본형 값도 객체로 다뤄져야 할 때가 있음
 		// collection: List 80% / Set 거의 안 쓰임 / Map 20%
 
-		// List: 순서 O , 데이터 중복 허용 O
-		// Set: 순서 X , 데이터 중복 허용 X / 순서가 없기 때문에 iterator를 사용해서 읽어야함!
+		// List: 순서 O , 데이터 중복 허용 O => 읽을 때 for문 사용 가능
+		// Set: 순서 X , 데이터 중복 허용 X => 순서가 없기 때문에 iterator를 사용해서 읽어야함!
 		// Map: 순서 X , 데이터 중복 허용 (Key X, Value O) / 값 넣을 때 put 사용! 
 		// 기능은 많지만 대부분 add, get만 쓰이고, 나머지는 오라클에서 수행.
 
 		// list: 객체들의 집합, 데이터 이동 및 읽기, 저장용도로 사용
-		ArrayList list = new ArrayList();
+		List list = new ArrayList();
+		// 다형성: 부모의 참조변수로 자손의 객체를 다루는 것
 		list.add("abc"); // 0
 		list.add("abc"); // 1 (순서 있음, 중복 허용)
 		list.add("bbb"); // 2
@@ -30,7 +32,7 @@ public class C0530_05_Collection_ArrayList {
 
 		//
 //		ArrayList list2 = new ArrayList();
-//		list2.add(new Student(1, "홍길동", 100, 100, 99));
+//		list2.add(new Student(1, "홍길동", 100, 100, 99)); // 객체를 넣어야함!
 //		list2.add(new Student(2, "유관순", 90, 90, 99));
 //		list2.add(new Student(3, "이순신", 80, 80, 87));
 //
