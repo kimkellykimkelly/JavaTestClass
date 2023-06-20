1f04f712c3ee742445affa11bc668474fa443f49
82106 <82106@DESKTOP-G434CV6>
2023-06-20 21:13:52 +0900

    0619

---
+++ jsp0619/src/main/java/com/java/www/Member.java
@@ -0,0 +1,48 @@
+package com.java.www;
+
+public class Member {
+	
+	//생성자
+	public Member(){}
+	public Member(String id, String pw, String name, String phone) {
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.phone = phone;
+	}
+	
+	//변수 선언
+	private String id;
+	private String pw;
+	private String name;
+	private String phone;
+	
+	//getter setter
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+
+
+}
